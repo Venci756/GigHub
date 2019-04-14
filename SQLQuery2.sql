bd3bba2da1b6ad0c8b55833db6908ae4b552f9c4
Venko Trajkovski <infidel756@gmail.com>
2019-04-14 15:56:00 +0200

    Populate Genres table

---
+++ SQLQuery2.sql
@@ -0,0 +1,10 @@
+﻿SET IDENTITY_INSERT Genres ON
+INSERT INTO [dbo].[Genres] (Id,Name)
+VALUES (1,'Jazz'); 
+INSERT INTO [dbo].[Genres] (Id,Name)
+VALUES (2,'Blues'); 
+INSERT INTO [dbo].[Genres] (Id,Name)
+VALUES (3,'Rock'); 
+INSERT INTO [dbo].[Genres] (Id,Name)
+VALUES (4,'Country'); 
+SET IDENTITY_INSERT Genres OFF
