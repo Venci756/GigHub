61e7bec346f71833e50b3743497efb19d12d482c
Venko Trajkovski <venko.trajkovski@gmail.com>
2019-04-14 20:23:52 +0200

    Create Add a Gig form.

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,1 +1,10 @@
-﻿SET IDENTITY_INSERT [dbo].[Genres] ON;
+﻿SET IDENTITY_INSERT Genres ON
+INSERT INTO [dbo].[Genres] (Id,Name)
+VALUES (1,'Jazz'); 
+INSERT INTO [dbo].[Genres] (Id,Name)
+VALUES (2,'Blues'); 
+INSERT INTO [dbo].[Genres] (Id,Name)
+VALUES (3,'Rock'); 
+INSERT INTO [dbo].[Genres] (Id,Name)
+VALUES (4,'Country'); 
+SET IDENTITY_INSERT Genres OFF
